6c134afb92bcd137da5e8d3fdcf22f3741aacf02
surrexi <surrexi03@gmail.com>
2014-09-23 00:17:28 +0600

    customize pg

--- db/migrate/20140922135337_create_users.rb
+++ db/migrate/20140922135337_create_users.rb
@@ -4,6 +4,6 @@ class CreateUsers < ActiveRecord::Migration
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :birthday, :string
-    add_column :users, :admin, :bollean, default: false
+    add_column :users, :admin, :boolean, default: false
   end
 end

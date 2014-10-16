9ed759487bc973d2bf448d2e2eb13f5952f10f76
surrexi <surrexi@surrexi-K43SD.(none)>
2014-10-16 14:34:21 +0600

    install ujs

--- Gemfile
+++ Gemfile
@@ -16,8 +16,6 @@ gem 'coffee-rails', '~> 4.0.0'
 
 gem 'jquery-rails'
 
-gem 'turbolinks'
-
 gem 'spring',        group: :development
 
 gem 'devise'
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,15 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource :photo
-  load_and_authorize_resource :comment, through: :photo
+  load_and_authorize_resource :comment
 
   def create
     @comment = @photo.comments.create(comment_params.merge(user: current_user))
-    redirect_to [@photo.album, @photo]
+    render @photo.comments
   end
 
   def destroy
     @comment.destroy
-    redirect_to [@photo.album, @photo]
+    render json: { id: @comment.id, message: "Comment was deleted" }
   end
 
   private
